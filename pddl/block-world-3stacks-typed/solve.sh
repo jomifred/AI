865764e6175106c2c1883de368c72b694e0d8d5c
Jomi Fred Hubner <jomifred@gmail.com>
2022-05-30 15:49:37 -0300

    update scripts

--- pddl/block-world-3stacks-typed/solve.sh
+++ pddl/block-world-3stacks-typed/solve.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-docker run -p 25000:5000 -t -v "$(pwd)":/x azathoth/pddl &
+if [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; then
+  docker run -p 25000:5000 -t -v "$(pwd)":/x azathoth/pddl &
+fi
+
+while [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; do
+  echo "waiting docker solver..."
+  sleep 1
+done
+
+
 # 41 steps
 #docker exec -it $(docker ps -qf "ancestor=azathoth/pddl" ) \
 #   /root/planners/FF/ff -o /x/domain-bw.pddl -f /x/problem-bw.pddl
--- pddl/jars/solve.sh
+++ pddl/jars/solve.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-docker run -p 25000:5000 -t -v "$(pwd)":/x azathoth/pddl &
+if [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; then
+  docker run -p 25000:5000 -t -v "$(pwd)":/x azathoth/pddl &
+fi
+
+while [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; do
+  echo "waiting docker solver..."
+  sleep 1
+done
+
 docker exec -it $(docker ps -qf "ancestor=azathoth/pddl" ) \
    /root/planners/Metric-FF/ff -o /x/domain-jars.pddl -f /x/problem-jars.pddl
