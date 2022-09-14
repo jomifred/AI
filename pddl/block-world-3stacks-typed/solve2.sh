f84ef6e2b6c588bafdf7f083061b6009dfc25e4d
Jomi Fred Hubner <jomifred@gmail.com>
2022-09-14 20:40:52 -0300

    Update solve2.sh

--- pddl/block-world-3stacks-typed/solve2.sh
+++ pddl/block-world-3stacks-typed/solve2.sh
@@ -1,3 +1,5 @@
+# from http://pddl4j.imag.fr/getting_started.html
+# and  http://pddl4j.imag.fr/running_planners_from_command_line.html
 
 java -cp ../lib/pddl4j-4.0.0.jar \
  fr.uga.pddl4j.planners.statespace.HSP \
@@ -7,6 +9,3 @@ java -cp ../lib/pddl4j-4.0.0.jar \
 #   fr.uga.pddl4j.planners.statespace.GSP \
 #   domain-bw.pddl problem-bw.pddl \
 #   -s ASTAR -e FAST_FORWARD
-
-# from http://pddl4j.imag.fr/getting_started.html
-# and  http://pddl4j.imag.fr/running_planners_from_command_line.html
