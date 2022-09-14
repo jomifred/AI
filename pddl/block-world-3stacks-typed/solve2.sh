4727e221528cb7ca17a7eba5b7e46323c124b17e
Jomi Fred Hubner <jomifred@gmail.com>
2022-09-14 18:56:42 -0300

    add pddl4

---
+++ pddl/block-world-3stacks-typed/solve2.sh
@@ -0,0 +1,8 @@
+
+java -cp ../lib/pddl4j-4.0.0.jar \
+   fr.uga.pddl4j.planners.statespace.HSP \
+   domain-bw.pddl problem-bw.pddl
+
+
+# from http://pddl4j.imag.fr/getting_started.html
+# and  http://pddl4j.imag.fr/running_planners_from_command_line.html
