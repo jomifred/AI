ee37aa81d4d83f7b0ed23e8799f6d978d13f3c5b
Jomi Fred Hubner <jomifred@gmail.com>
2025-10-06 10:01:59 -0300

    Update solve.sh

--- pddl/voo2/solve.sh
+++ pddl/voo2/solve.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; then
-  docker run -p 25000:5000 -t -v "$(pwd)":/x azathoth/pddl &
+if [ ! `docker ps -qf "ancestor=azathoth/pddl-temporal"` ]; then
+  docker run -p 25000:5001 -t -v "$(pwd)":/x azathoth/pddl-temporal &
 fi
 
-while [ ! `docker ps -qf "ancestor=azathoth/pddl"` ]; do
+while [ ! `docker ps -qf "ancestor=azathoth/pddl-temporal"` ]; do
   echo "waiting docker solver..."
   sleep 1
 done
 
 
-docker exec -it $(docker ps -qf "ancestor=azathoth/pddl" ) \
-   /root/planners/optic-clp /x/domain.pddl /x/problem.pddl
+# Optic find solution with cost 13
+#docker exec -it $(docker ps -qf "ancestor=azathoth/pddl-temporal" ) \
+#   /root/planners/optic-clp /x/domain.pddl /x/problem.pddl
+
+docker exec -it $(docker ps -qf "ancestor=azathoth/pddl-temporal" ) \
+   /root/planners/temporal/popf3-clp -e /x/domain.pddl /x/problem.pddl
